89953cb5381457c6749f525784130abb2363b023
mramirez092 <miguelramirez2210@gmail.com>
2023-04-05 19:22:30 +0000

    Pryecto con implementacion de estados

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,29 +1,40 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
+import '../../styles/index.css';
 
 //create your first component
 const ToDoList = () => {
-	return (
-			<div className="container-fluid">
-				<div className=" d-flex header m-3 justify-content-center">
-					<h1>Reminders</h1>
-				</div>
-				<div className="input m-4">
-				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="What needs to be done?"/>
-				</div>
-				<div className="list m-4">
-				<ul class="list-group">
-  					<li class="list-group-item">An item</li>
-  					<li class="list-group-item">A second item</li>
-  					<li class="list-group-item">A third item</li>
-  					<li class="list-group-item">A fourth item</li>
-  					<li class="list-group-item">And a fifth one</li>
-				</ul>
-				</div>
-			</div>
-	);
+
+    const [currentTask, setCurrentTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const handleKeyDown = (e) => {
+        if (e.keyCode === 13 && currentTask !== "") {
+            setTasks([...tasks, currentTask]);
+            setCurrentTask("");
+        }
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className=" d-flex header m-3 justify-content-center">
+                <h1>Reminders</h1>
+            </div>
+            <div className="d-flex input m-4 justify-content-center">
+                <input type="text" onChange={(e) => setCurrentTask(e.target.value)} onKeyDown={handleKeyDown} className="taskInput" placeholder="What needs to be done?"/>
+            </div>
+            <div className="d-flex button-container justify-content-center">
+                <button onClick={() => setTasks([...tasks, currentTask])} type="button" className={`button ${currentTask.length == 0 && "d-none"}`}>Remind me !</button>
+            </div>
+            <div className="list m-4">
+                <ul className="list-group">
+                    {tasks.map((t) => (
+                        <li class="list-group-item">{t} - {t.length}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default ToDoList;
