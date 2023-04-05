d1cab7e1c5c32f94709518b7f2833b414b423446
mramirez092 <miguelramirez2210@gmail.com>
2023-04-05 17:57:24 +0000

    Comienzo del proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,14 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const ToDoList = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container-fluid">
+				
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default ToDoList;
