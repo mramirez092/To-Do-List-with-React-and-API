6320e62e48350247b4e84fa68f9f7f7cf67e4062
mramirez092 <miguelramirez2210@gmail.com>
2023-04-15 16:34:46 +0000

    Proyecto Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import '../../styles/index.css';
 import {AiOutlineDelete} from 'react-icons/ai';
+import { element } from "prop-types";
 
 //create your first component
 const ToDoList = () => {
 
     const [currentTask, setCurrentTask] = useState("");
     const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getAllTasks();
+    }, []);
 
     const removeItem = (taskId) => {
         const newTasks = tasks.filter( (t, index) => index !== taskId );
@@ -17,9 +22,66 @@ const ToDoList = () => {
     const handleKeyDown = (e) => {
         if (e.keyCode === 13 && currentTask !== "") {
             setTasks([...tasks, currentTask]);
-            setCurrentTask("");
+            handleAddTask("");
         }
     }
+
+    //FUNCION GET//
+
+    const getAllTasks = () => {
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/mramirez')
+        .then((response) => response.json())
+		.then((data) => {
+			setTasks(data)
+			console.log(data)})
+		.catch(error => console.log(error))
+    }
+
+    //FUNCION PUT//
+
+    const handleAddTask = () => {
+        const data = tasks.concat({
+            "label": currentTask,
+            "done": false
+        });
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/mramirez', {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data);
+        getAllTasks(); 
+    })
+    .catch(error => console.log(error));
+    }
+
+    //FUNCION DELETE//
+
+    const deleteElement = (element) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        let eliminar = tasks.filter(task => task !== element)
+        console.log(eliminar);
+
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: JSON.stringify(eliminar),
+            redirect: 'follow'
+        };
+
+        fetch('https://assets.breatheco.de/apis/fake/todos/user/mramirez', requestOptions)
+            .then(response => response.json())
+            .then(result => getAllTasks())
+            .catch(error => console.log('error', error));
+    }
+
+
 
     return (
     <div className="container-fluid">
@@ -28,15 +90,14 @@ const ToDoList = () => {
         </div>
         <div className="d-flex input m-4 justify-content-center">
             <div className="inputbox">
-            <input required="required" type="text" onChange={(e) => setCurrentTask(e.target.value)} onKeyDown={handleKeyDown}/>
+            <input required="required" type="text" onChange={(e) => setCurrentTask(e.target.value)} onKeyDown={handleKeyDown} value={currentTask}/>
                  <span><b>What needs to be done?</b></span>
              <i></i>
-</div>
+    </div>
         </div>
         <div className="d-flex button-container flex-column align-items-center">
-            <button onClick={() => setTasks([...tasks, currentTask])} type="button" className={`button ${currentTask.length === 0 && "d-none"}`}>
-                Remind me !
-            </button>
+    
+            <button onClick={handleAddTask}>Add Item</button>
             {tasks.length === 0 && (
                 <div className="temporal-title">
                     <h2>No tasks yet. Start adding some!</h2>
@@ -45,11 +106,15 @@ const ToDoList = () => {
         </div>
         {tasks.length !== 0 && (
             <div className="list m-4">
-                <ul className="list-group">
+                <ul className="list-group">                 
                     {tasks.map((t, index) => (
-                        <li key={index} className="list-group-item"> {t} - <strong>{t.length}</strong> <AiOutlineDelete onClick={(event)=>{
-                            removeItem(index);
-                        }} className="icono-task"/> 
+                        <li key={index} className="list-group-item"> 
+                            {t.label}
+                            
+                            <AiOutlineDelete 
+                            onClick={() => deleteElement(t)} 
+                            className="icono-task" /> 
+
                         </li>
                     ))}
                 </ul>
